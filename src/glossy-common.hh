8d88dd192c042ffa7452d5586bb0258cf7ecd297
Behdad Esfahbod <behdad@behdad.org>
2012-02-03 18:58:33 -0500

    Add glossy-common.h
    Oops.

---
+++ src/glossy-common.hh
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2012 Google, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Google Author(s): Behdad Esfahbod, Maysum Panju
+ */
+
+#ifndef GLOSSY_COMMON_HH
+#define GLOSSY_COMMON_HH
+
+#include <math.h>
+#include <assert.h>
+
+#define GLOSSY_ARRAY_LENGTH(__array) ((signed int) (sizeof (__array) / sizeof (__array[0])))
+
+#define _GLOSSY_ASSERT_STATIC1(_line, _cond) typedef int _static_assert_on_line_##_line##_failed[(_cond)?1:-1]
+#define _GLOSSY_ASSERT_STATIC0(_line, _cond) _GLOSSY_ASSERT_STATIC1 (_line, (_cond))
+#define GLOSSY_ASSERT_STATIC(_cond) _GLOSSY_ASSERT_STATIC0 (__LINE__, (_cond))
+
+
+#endif /* GLOSSY_COMMON_HH */
